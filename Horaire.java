3a52e81013d1cea4e26b5e3b3c789df3e6e8cca9
Noa Fonteny <noatoimoi@gmail.com>
2024-02-22 09:14:48 +0100

    creation horaire

---
+++ Horaire.java
@@ -0,0 +1,9 @@
+public class Horaire{
+    private String startDate;
+    private String endDate;
+
+    public Horaire(String start, String end){
+        this.startDate=start;
+        this.endDate=end;
+    } 
+}
