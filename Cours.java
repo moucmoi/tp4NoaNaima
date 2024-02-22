b8170ba572c3cf78551fc43f153b40366a99e612
Naima Akhtar <naima.akhtar@etu.univ-orleans.fr>
2024-02-22 10:43:42 +0100

    repar

---
+++ Cours.java
@@ -0,0 +1,13 @@
+public class Cours{
+    private Horaire horaire;
+    private Groupe groupe;
+    private Matiere matiere;
+    private User user;
+
+    public Cours(Horaire horaire,Groupe groupe,Matiere matiere,User user){
+        this.horaire=horaire;
+        this.groupe=groupe;
+        this.matiere=matiere;
+        this.user=user;
+    }
+}
