7d3a3181c22144865036eef3466e0c9c35d83e8a
Noa Fonteny <noatoimoi@gmail.com>
2024-03-14 09:16:25 +0100

    setter user et modif user

--- Cours.java
+++ Cours.java
@@ -2,13 +2,13 @@ public class Cours{
     private Horaire horaire;
     private List<Group> groupe;
     private List<Matiere> matiere;
-    private List<User> user;
+    private User user;
 
     public Cours(Horaire horaire,Groupe groupe,Matiere matiere,User user){
         this.horaire=horaire;
         this.groupe=new ArrayList<>();
         this.matiere=new ArrayList<>();
-        this.user=new ArrayList<>();
+        this.user=user;
     }
 
     public Horaire getHoraire() {
@@ -49,9 +49,5 @@ public class Cours{
 
     public void ajouteMatiere(Matiere matiere){
         this.matiere.add(matiere);
-    }
-
-    public void ajouteUser(User user){
-        this.user.add(user);
     }
 }
--- User.java
+++ User.java
@@ -2,11 +2,13 @@ public class User{
     private String nom;
     private String prenom;
     private String dateDeNaissance;
+    private List<Cours> cours;
 
     public User(String nom, String prenom, String dateDeNaissance){
         this.nom = nom;
         this.prenom = prenom;
         this.dateDeNaissance = dateDeNaissance;
+        this.cours=new ArrayList<>();
     }
 
     public String getNom(){
@@ -19,5 +21,21 @@ public class User{
 
     public String getDateDeNaissance(){
         return this.dateDeNaissance;
+    }
+
+    public void setNom(String nom){
+        return this.nom=nom;
+    }
+
+    public void setPrenom(String prenom){
+        return this.prenom=prenom;
+    }
+
+    public void setDateDeNaissance(String dateDeNaissance){
+        return this.dateDeNaissance=dateDeNaissance;
+    }
+
+    public void ajouteCours(Cours cours){
+        this.cours.add(cours);
     }
 }
