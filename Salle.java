dfac8bc1d3c85a7c3c54cac3b28d34ac26af1934
moucmoi <143808893+moucmoi@users.noreply.github.com>
2024-03-14 09:29:38 +0100

    getter setter et constructeur salle (#22)

---
+++ Salle.java
@@ -0,0 +1,23 @@
+public class Salle{
+    private int numéro;
+    private Horaire horaire;
+    
+    public Salle(int numéro, Horaire horaire) {
+        this.numéro = numéro;
+        this.horaire = horaire;
+    }
+    public int getNuméro() {
+        return numéro;
+    }
+    public void setNuméro(int numéro) {
+        this.numéro = numéro;
+    }
+    public Horaire getHoraire() {
+        return horaire;
+    }
+    public void setHoraire(Horaire horaire) {
+        this.horaire = horaire;
+    }
+
+
+}
