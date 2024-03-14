d99db96ffebd7f6bdfbfb72dfcc5d07207e84586
moucmoi <143808893+moucmoi@users.noreply.github.com>
2024-03-14 10:08:03 +0100

    Etudiant (#24)
    * changement cours
    
    * List de groupe et liste de notes

--- Etudiant.java
+++ Etudiant.java
@@ -1,12 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Etudiant {
     private String nom;
     private String prenom;
     private String dateDeNaissance;
+    private List<Groupe> groupe;
+    private List<Note> notes;
 
     public Etudiant(String nom, String prenom, String dateDeNaissance){
         this.nom = nom;
         this.prenom = prenom;
         this.dateDeNaissance = dateDeNaissance;
+        this.groupe=new ArrayList<>();
+        this.notes=new ArrayList<>();
     }
 
     public String getNom(){
@@ -31,5 +38,13 @@ public class Etudiant {
 
     public void setDateDeNaissance(String dateDeNaissance) {
         this.dateDeNaissance = dateDeNaissance;
+    }
+
+    public void ajouteGroupe(Groupe groupe){
+        this.groupe.add(groupe);
+    }
+
+    public void ajouteNote(Note note){
+        this.notes.add(note);
     }
 }
