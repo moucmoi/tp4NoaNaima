7d3b4deddf82d52fabbde9d900155078ae931286
moucmoi <143808893+moucmoi@users.noreply.github.com>
2024-03-14 10:23:46 +0100

    ajout note dans matiere + getter et setter dans groupe (#27)

--- Matiere.java
+++ Matiere.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 public class Matiere {
     private String nomMatiere;
     private List<Cours> lesCours;
+    private List<Note> notes;
 
     public Matiere(String nom){
         this.nomMatiere = nom;
         this.lesCours = new ArrayList<>();
+        this.notes = new ArrayList<>();
     }
 
     public String getNomMatiere(){
@@ -22,11 +24,11 @@ public class Matiere {
         return this.lesCours;
     }
 
-    public void setLesCours(List<Cours> lesCours) {
-        this.lesCours = lesCours;
-    }
-
     public void ajouteCours(Cours cours){
         this.lesCours.add(cours);
+    }
+
+    public void ajouteNote(Note note){
+        this.notes.add(note);
     }
 }
