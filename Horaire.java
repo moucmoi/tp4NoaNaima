63937925cacc611a53894497e0a314041bfb0108
moucmoi <143808893+moucmoi@users.noreply.github.com>
2024-03-14 10:19:18 +0100

    rajout salle et cours dans horaire (#26)

--- Horaire.java
+++ Horaire.java
@@ -1,10 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Horaire{
     private String startDate;
     private String endDate;
+    private List<Cours> cours;
+    private List<Salle> salle;
 
     public Horaire(String start, String end){
         this.startDate = start;
         this.endDate = end;
+        this.cours=new ArrayList<>();
+        this.salle=new ArrayList<>(); 
     }
 
     public String getStartDate() {
@@ -21,5 +28,13 @@ public class Horaire{
 
     public void setEndDate(String endDate) {
         this.endDate = endDate;
+    }
+
+    public void ajouteCours(Cours cours){
+        this.cours.add(cours);
+    }
+
+    public void ajouteSalle(Salle salle){
+        this.salle.add(salle);
     }
 }
