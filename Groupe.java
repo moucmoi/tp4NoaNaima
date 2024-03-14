ac05e5542c113c3a5300829c531c45cb8ae09790
Naaimaaa <116354617+Naaimaaa@users.noreply.github.com>
2024-03-14 09:19:07 +0100

    reparations d'erreurs (#19)
    Co-authored-by: Naima Akhtar <naima.akhtar@etu.univ-orleans.fr>

--- Groupe.java
+++ Groupe.java
@@ -1,28 +1,24 @@
-Import java.util.List;
-Import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Groupe{
     private List<Etudiant> etudiant;
     private List<Cours> cours;
 
     public Groupe(){
-        this.etudiant=new ArrayList<>();
-        this.cours=new ArrayList<>();
+        this.etudiant = new ArrayList<>();
+        this.cours = new ArrayList<>();
     }
 
     public void ajouterEtudiant(Etudiant etudiant){
         this.etudiant.add(etudiant);
     }
 
-    public void ajouterEtudiant(Cours cours){
-        this.etudiant.add(cours);
-    }
-
-    public List getEtudiant(){
+    public List<Etudiant> getEtudiant(){
         return this.etudiant;
     }
 
-    public List getCours{
+    public List<Cours> getCours(){
         return this.cours;
     }
 }
